304e7a502802e55b0bfc8656d674f3b8dfed8b44
jchi <jchi777777@gmail.com>
2022-01-21 16:44:59 +0800

    js

--- js/checkSlide.js
+++ js/checkSlide.js
@@ -1,8 +1,8 @@
 function checkSlide(){
     const targets = document.querySelectorAll('.normal');
     targets.forEach(tar => {
-        const standard = (window.innerHeight + window.scrollY) - tar.height / 2;
-        const tarBottom = tar.offsetTop + tar.height;
+        const standard = (window.innerHeight + window.scrollY) - tar.offsetHeight / 2;
+        const tarBottom = tar.offsetTop + tar.offsetHeight;
         const isHalf = standard > tar.offsetTop;
         const isNotPast = window.scrollY < tarBottom;
         if(isHalf && isNotPast){
