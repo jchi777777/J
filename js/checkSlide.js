ad68cdb530885539451b3e9ef1ded0b37af3f472
jchi <jchi777777@gmail.com>
2022-01-21 16:00:23 +0800

    slide

--- js/checkSlide.js
+++ js/checkSlide.js
@@ -1,12 +1,12 @@
 function checkSlide(){
-    const images = document.querySelectorAll('img');
-    images.forEach(img => {
-        const ifshow = img.offsetTop + (img.offsetHeight / 1.5);
+    const targets = document.querySelectorAll('.normal');
+    targets.forEach(tar => {
+        const ifshow = tar.offsetTop + (tar.offsetHeight / 1.5);
         let standard = window.innerHeight + window.scrollY;
         if(standard > ifshow){
-            img.classList.add('slide_ani');
+            tar.classList.add('slide_ani');
         }else{
-            img.classList.remove('slide_ani');
+            tar.classList.remove('slide_ani');
         }
     })
 }
